226e005530b7b9dee8ae62a36f65aec10f80f0e6
Поздеев Максим Львович <pozdeevml@sibintek.ru>
2021-08-09 14:43:59 +0400

    Перенес MainActivity в другой пакет

--- WheatherApp/app/src/main/java/com/example/wheatherapp/presentation/activities/MainActivity.kt
+++ WheatherApp/app/src/main/java/com/example/wheatherapp/presentation/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.wheatherapp
+package com.example.wheatherapp.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wheatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
